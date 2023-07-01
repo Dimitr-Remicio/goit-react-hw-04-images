36ac81df89f253095050f1092f013a6d810b2eee
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-07-01 17:30:03 -0500

    cloning archives

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import css from './ImageGallery.module.scss'
+
+export const ImageGallery = ({children}) => {
+    return (
+        <ul className={css["imageGallery"]}>
+          {children}
+        </ul>
+    )
+  }
+
+
+ImageGallery.propTypes = {
+  children: PropTypes.node
+}
